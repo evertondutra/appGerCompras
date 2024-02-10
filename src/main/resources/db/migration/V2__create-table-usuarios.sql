18296af705ccdca05bba7f9855e309693bddcc1c
Everton Dutra <evertondutra02@gmail.com>
2024-02-10 00:16:19 -0300

    AP criado a coluna create_date

--- src/main/resources/db/migration/V2__create-table-usuarios.sql
+++ src/main/resources/db/migration/V2__create-table-usuarios.sql
@@ -4,6 +4,7 @@ create table usuarios(
     nome varchar(30) not null,
     email varchar(100) not null unique,
     senha varchar(20) not null,
+    create_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     primary key(id)
 
