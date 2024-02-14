0c850bad85b9595b32db5dbddc8240033ad6bf71
Everton Dutra <evertondutra02@gmail.com>
2024-02-14 02:06:05 -0300

    AP-8 adicionado mpaginação no método que busca usuários por nível

--- src/main/java/com/market/appGerCompras/controller/UserController.java
+++ src/main/java/com/market/appGerCompras/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.market.appGerCompras.controller;
 
-import com.market.appGerCompras.usuario.DadosCadastroUsuario;
-import com.market.appGerCompras.usuario.DadosListagemUsuario;
-import com.market.appGerCompras.usuario.Usuario;
-import com.market.appGerCompras.usuario.UsuarioRepository;
+import com.market.appGerCompras.usuario.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,22 +25,27 @@ public class UserController {
     }
 
     @GetMapping
-    public List<DadosListagemUsuario> listar(){
+    public Page<DadosListagemUsuario> listar(Pageable paginacao){
         // converte uma lista de usuários para uma lista de dadosListagemMusuarios
-        return repository.findAll().stream().map(DadosListagemUsuario::new).toList();
+        return repository.findAll(paginacao).map(DadosListagemUsuario::new);
     }
+
+//    @GetMapping("/{nivel}")
+//    public List<DadosListagemUsuario> listarUsuarioNivel(@PathVariable String nivel) {
+//        // Converte uma lista de usuários para uma lista de dadosListagemMusuarios
+//        return repository.findAll().stream()
+//                .filter(usuario -> nivel.equalsIgnoreCase(usuario.getNivel().name())) // Comparação com o nome do Enum
+//                .map(DadosListagemUsuario::new)
+//                .collect(Collectors.toList());
+//    }
 
     @GetMapping("/{nivel}")
-    public List<DadosListagemUsuario> listarUsuarioNivel(@PathVariable String nivel) {
-        // Converte uma lista de usuários para uma lista de dadosListagemMusuarios
-        return repository.findAll().stream()
-                .filter(usuario -> nivel.equalsIgnoreCase(usuario.getNivel().name())) // Comparação com o nome do Enum
-                .map(DadosListagemUsuario::new)
-                .collect(Collectors.toList());
+    public Page<DadosListagemUsuario> listarUsuarioNivel(@PathVariable Nivel nivel, Pageable paginacao) {
+        // Use o método do repositório para buscar usuários por nível e aplicar paginação
+        Page<Usuario> usuariosPorNivel = repository.findByNivel(nivel, paginacao);
+
+        // Converta a Page de usuários para uma Page de DadosListagemUsuario
+        return usuariosPorNivel.map(DadosListagemUsuario::new);
     }
-
-
-
-
 
 }
--- src/main/java/com/market/appGerCompras/usuario/UsuarioRepository.java
+++ src/main/java/com/market/appGerCompras/usuario/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package com.market.appGerCompras.usuario;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Page<Usuario> findByNivel(Nivel nivel, Pageable pageable);
 }
 
 
